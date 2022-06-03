7384886ae9a441099d7ed539c1197a8106a13575
KirillovItstep <kirillov.malp@gmail.com>
2022-06-03 16:10:48 +0300

    Layouts - ё эєьхЁрЎшхщ шыш схч

---
+++ github.bat
@@ -0,0 +1,8 @@
+cd /d "D:\Matlab\Pak2Vector\Layouts\Layouts\"
+git init
+git add .
+git commit -m "Layouts - с нумерацией или без"
+git remote add origin https://github.com/KirillovItstep/Layouts
+git push -u origin
+
+git config --global user.name "kirillovItStep"
